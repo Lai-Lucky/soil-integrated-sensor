0f158858151519f10611c65e41a72bf95a67a9a3
Lai-Lucky <2086287037@qq.com>
2025-07-07 11:53:09 +0800

    v4.1:完整版

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #include <freertos/task.h>     // 添加 FreeRTOS 任务管理头文件
 #include <freertos/semphr.h>   // 添加 FreeRTOS 信号量头文件
 
+#define M0 23 //M0控制引脚
+#define M1 22 //M1控制引脚
+
 
 /************** 函数声明 ***************/
 uint16_t CRC16(const uint8_t *data, uint16_t length);
@@ -46,7 +49,15 @@ const char* lcd_names[] = {"x0"/*酸碱度*/,
 
 
 /************ OneNet平台的属性标识符 ************/
-const char* sensor_names[] = {"soil-PHM"/*酸碱度*/, 
+const char* sensor_names[] = {"soil-PH"/*酸碱度*/, 
+                              "soil-T"/*温度*/, 
+                              "soil-H"/*湿度*/, 
+                              "soil-N"/*氮*/, 
+                              "soil-P"/*磷*/, 
+                              "soil-K"/*钾*/};
+
+/************ OneNet平台的属性标识符(移动版) ************/
+const char* sensor_names_M[] = {"soil-PHM"/*酸碱度*/, 
                               "soil-TM"/*温度*/, 
                               "soil-HM"/*湿度*/, 
                               "soil-NM"/*氮*/, 
@@ -61,7 +72,11 @@ int asr = 0;  // 传感器轮询索引
 
 /************* 程序初始化 *************/
 void setup() {
-  Serial.begin(9600);
+
+  pinMode(22,OUTPUT);
+  pinMode(23,OUTPUT);
+  
+  Serial.begin(115200);
   Serial2.begin(9600);
 
   xTaskCreate(soilsensor_task_vtask,"soilsensor_task_vtask",4096,NULL,1,&soilsensor_task_handle);//创建土壤传感器任务
@@ -100,7 +115,7 @@ bool checkCRC(const uint8_t *data, uint16_t len) {
   return computedCRC == receivedCRC;
 }
 
-
+  
 
 /************* 解析数据并上传 *************/
 void parseModbusData(const uint8_t *data, uint16_t len) {
@@ -134,7 +149,6 @@ void parseModbusData(const uint8_t *data, uint16_t len) {
 void sendSensorData(double data) 
 {
   ZigBeec_controller(1);
-  vTaskDelay(pdMS_TO_TICKS(100));
   JsonDocument doc;
   doc["id"] = String(millis());  // 使用时间戳作为唯一ID
   doc["version"] = "1.0";
@@ -144,13 +158,13 @@ void sendSensorData(double data)
   serializeJson(doc, payload);
 
   Serial.println(payload.c_str());
+  vTaskDelay(pdMS_TO_TICKS(1000));
 
   ZigBeec_controller(0);
-  vTaskDelay(pdMS_TO_TICKS(100));
 
   Serial.printf("\xff\xff\xff");
   data*=10.0;
-  Serial.printf("%s.val=%d\xff\xff\xff",lcd_names[asr],(int)data);
+  Serial.printf("%s.val=%d\xff\xff\xff\n",lcd_names[asr],(int)data);
   vTaskDelay(pdMS_TO_TICKS(100));
 }
 
@@ -160,13 +174,15 @@ void sendSensorData(double data)
 void ZigBeec_controller(int switchs){
   if(switchs==1)
   {
-    digitalWrite(34,HIGH);//M1
-    digitalWrite(35,HIGH);//M0
+    digitalWrite(M1,LOW);//M1
+    digitalWrite(M0,HIGH);//M0
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
   else
   {
-    digitalWrite(34,LOW);
-    digitalWrite(35,HIGH);
+    digitalWrite(M1,HIGH);//M1
+    digitalWrite(M0,HIGH);//M0
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 
 }
@@ -198,5 +214,5 @@ void soilsensor_task_vtask(void *pv){
     vTaskDelay(pdMS_TO_TICKS(500));
     }
   }
+  
 }
-
